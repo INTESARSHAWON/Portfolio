70c9ae8fabf19edb9740aa937074c8066ff2f540
INTESARSHAWON <intesarshawon@gmail.com>
2024-09-12 20:09:27 +0600

    send contact form data on email

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,6 +7,32 @@ import location_icon from '../../assets/location_icon.svg';
 
 
 const Contact = () => {
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "3f916b31-05ba-49a0-8af1-065ea0556443");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          console.log("Success", res);
+          alert(res.message);
+        }
+      };
+
+
   return (
     <div id='contact' className='contact'>
         <div className='contact-title'>
@@ -32,7 +58,7 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-            <form className='contact-right'>
+            <form onSubmit={onSubmit} className='contact-right'>
                 <label htmlFor=''>Your Name</label>
                 <input type='text' placeholder='Enter Your Name' name='name'/>
                 <label htmlFor=''>Your Email</label>
