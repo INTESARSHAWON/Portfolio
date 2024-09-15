99a7b89a5e8c8378536b42ae764f2a439d06e2d7
INTESARSHAWON <intesarshawon@gmail.com>
2024-09-16 00:46:51 +0600

    navbar responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 import './Navbar.css';
 import intesar from '../../assets/intesar.png';
 import underline from '../../assets/nav_underline.svg';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import menu_open from '../../assets/menu_open.svg';
 import menu_close from '../../assets/menu_close.svg';
@@ -10,13 +10,22 @@ import menu_close from '../../assets/menu_close.svg';
 const Navbar = () => {
 
   const [menu, setMenu] = useState('home');
+  const menuRef = useRef();
+
+  const openMenu = () =>{
+    menuRef.current.style.right = "0";
+  }
+
+  const closeMenu = () =>{
+    menuRef.current.style.right = "-450px";
+  }
 
   return (
     <div className='navbar'>
-        <img src={intesar} alt='logo'/>
-        <img className='nav-mob-open' src={menu_open} alt='menu_open'/>
-        <ul className='nav-menu'>
-            <img className='nav-mob-close' src={menu_close} alt='menu_close' />
+        <img src={intesar} alt='logo' onClick={()=> setMenu('home')} />
+        <img className='nav-mob-open' src={menu_open} alt='menu_open' onClick={openMenu}/>
+        <ul ref={menuRef} className='nav-menu'>
+            <img className='nav-mob-close' src={menu_close} alt='menu_close' onClick={closeMenu} />
             <li><AnchorLink className='anchor-link' href='#home'><p onClick={()=> setMenu('home')}>Home</p></AnchorLink>{menu==='home' ? <img src={underline} alt='underline'/> : <></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=> setMenu('about')}>About Me</p></AnchorLink>{menu === 'about' ? <img src={underline} alt='underline'/> : <></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=> setMenu('services')}>Services</p></AnchorLink>{menu === 'services' ? <img src={underline} alt='underline'/> : <></>}</li>
