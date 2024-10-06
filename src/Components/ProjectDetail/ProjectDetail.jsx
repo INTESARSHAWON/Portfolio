f2b149d59c94a3dbaeee4369c12d1db6495d03c5
INTESARSHAWON <intesarshawon@gmail.com>
2024-10-06 19:31:02 +0600

    technology used and sticky back

--- src/Components/ProjectDetail/ProjectDetail.jsx
+++ src/Components/ProjectDetail/ProjectDetail.jsx
@@ -40,7 +40,7 @@ const ProjectDetail = () => {
         title: "React Auth Cripto Dashboard",
         screenshots: [dashboard, transactions, support, signup, signin, forgotpassword],
         projectdetails: "This is a responsive crypto dashboard app created by React where I implemented authentication. There is a signin page, a signup page, along with a dashboard with transactions and support system. A user can sign in or sign out from the dashboard. Here I use Chakra UI as well.",
-        features: ["1.React", " ", "2.Chakra UI", " ", "3.JS", " ", "4.CSS"],
+        technologyUsed: ["React", ", ", "Chakra UI", ", ", "JS", ", ", "CSS", ", ", "html"],
         livelink: "www.facebook.com",
         gitlink: "www.facebook.com",
       },
@@ -49,7 +49,7 @@ const ProjectDetail = () => {
         title: "Dice Games (React)",
         screenshots: [dashboard2, playNow, playingGame, showRules],
         projectdetails: "its a Dice Game made with React. First user have to click play game to open the game. Then user have to select a number and roll the dice. If the number matches, user will get two point. If it doesn't then two points will be decreased. User can check total win or total lose in this game. Also user can check the rules and reset the game.",
-        features: ["1.React", " ", "2.JS", " ", "3.CSS"],
+        technologyUsed: ["React", ", ", "JS", ", ", "CSS", ", ", "html"],
         livelink: "www.facebook.com",
         gitlink: "www.facebook.com",
       },{
@@ -57,7 +57,7 @@ const ProjectDetail = () => {
         title: "React Foody Zone",
         screenshots: [foodyzone, breakfast, lunch, dinner, searchoption],
         projectdetails: "A food related responsive webpage where all the data comes from backend. Here I use Api calling function. Here A user can search food from the list.",
-        features: ["1.React", " ", "2.Styled components", " ", "3.JS"],
+        technologyUsed: ["React", ", ", "Styled components", ", ", "JS", ", ", "CSS", ", ", "html"],
         livelink: "www.facebook.com",
         gitlink: "www.facebook.com",
       },{
@@ -65,7 +65,7 @@ const ProjectDetail = () => {
         title: "E-Commerce (JS)",
         screenshots: [mainWebsite, shopPage, shoppingPage, blogPage, aboutPage, contactPage],
         projectdetails: "This is a reponsive e-commerce site where a customer can buy products, add to cart, read some blogs, know about the site and can contact with the organization.",
-        features: ["1.JS", " ", "2.CSS", " ", "3.html"],
+        technologyUsed: ["JS", ", ", "CSS", ", ", "html"],
         livelink: "www.facebook.com",
         gitlink: "www.facebook.com",
       },{
@@ -73,7 +73,7 @@ const ProjectDetail = () => {
         title: "Amazon Clone",
         screenshots: [mainWebsite2],
         projectdetails: "This is a clone of the Amazon website. Its very simple. Only CSS and html are used here.",
-        features: ["1.CSS", " ", "2.html"],
+        technologyUsed: ["CSS", ", ", "html"],
         livelink: "www.facebook.com",
         gitlink: "www.facebook.com",
       },{
@@ -81,7 +81,7 @@ const ProjectDetail = () => {
         title: "Sliding Album (JS)",
         screenshots: [slidingalbum, slidingalbum2],
         projectdetails: "a sliding album created by JS and css where user can see the photos on a sliding album.",
-        features: ["1.JS", " ", "2.CSS"],
+        technologyUsed: ["JS", ", ", "CSS", ", ", "html"],
         livelink: "www.facebook.com",
         gitlink: "www.facebook.com",
       },
@@ -105,7 +105,7 @@ const ProjectDetail = () => {
   
   return (
     <div className=''>
-        <div>
+        <div className='sticky-button'>
           <Link className= 'back-button' to="/#mywork"> <div> Back </div> </Link>
         </div>
         {isLoading ? <h1>Loading</h1> : <div className='projectDetail'>
@@ -118,7 +118,7 @@ const ProjectDetail = () => {
               }
           </div>
           <div className='myworkFeatures'>
-              <p>Features- {project.features}</p>
+              <p>Technology Used- {project.technologyUsed}</p>
           </div>
           <p>Livelink- {project.livelink}</p>
           <p>Gitlink- {project.gitlink}</p>
