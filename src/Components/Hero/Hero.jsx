6f90e71a5449a3bec4291a5b80b15e175bb530b6
INTESARSHAWON <intesarshawon@gmail.com>
2024-10-25 21:49:01 +0600

    My Resume setup in target _blank

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,15 +5,22 @@ import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
+
 const Hero = () => {
+  const onMyResumeClick = () => {
+    window.open('https://drive.google.com/file/d/1PreZfUqYAmEIrZtiXuUjWNY12inG776s/view?usp=drive_link','_blank');
+};
+
+  
+
   return (
     <div id='home' className='hero'>
         <img src={Intesar_Alam_Shawon} alt='Intesar_Alam_Shawon' />
         <h1><span>I am Intesar Shawon</span>, <b> Frontend Developer based in Bangladesh </b></h1>
         
         <div className= 'hero-action'>
-            <div className= 'hero-resume'>
-                My Resume
+            <div className= 'hero-resume' onClick={onMyResumeClick}>
+               My Resume  
             </div>
             <Link className= 'hero-github' to="https://github.com/INTESARSHAWON" target='_blank'> <div> <FaGithub/> Github </div> </Link>
             <Link className= 'hero-linkedin' to="https://www.linkedin.com/in/intesar-shawon-292752179/" target='_blank'> <div> Linked<FaLinkedin/> </div> </Link>
