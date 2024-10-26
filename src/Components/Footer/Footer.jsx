393915312e89e59e6c790e4a2aff6c205a0e6102
INTESARSHAWON <intesarshawon@gmail.com>
2024-10-26 14:35:29 +0600

    footer setup

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className='footer-top'>
             <div className='footer-top-left'>
                 <img src={intesar} alt='footer_logo'/>
-                <p>I am Intesar Shawon, Frontend Developer based in Bangladesh</p>
+                {/* <p>I am Intesar Shawon, Frontend Developer based in Bangladesh</p> */}
             </div>
             <div className='footer-top-right'>
                 <div className='footer-email-input'>
